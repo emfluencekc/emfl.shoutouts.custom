8607a3fa6a7e603b921ba52ed6ffb0f7f14360e8
jschwartz <jschwartz@emfluence.com>
2017-10-27 09:07:08 -0500

    Add viewport meta tag

--- display.php
+++ display.php
@@ -4,6 +4,7 @@ require_once 'inc/common.functions.php';
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>All the &hearts;</title>
   <link rel="stylesheet" href="css/display.css" />
 </head>
